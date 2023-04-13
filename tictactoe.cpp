710ddfa3ad1200d1d32ecba9570c997536f63b76
Aditya-U7 <66748266+Aditya-U7@users.noreply.github.com>
2023-04-13 21:36:27 +0530

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 using namespace std;
 
-char board[3][3]={{'1','2','3'},
-	{'4','5','6'},
-	{'7','8','9'}};
-string player1,player2;
-int p1_input,p2_input;
-int row,col;
+char board[3][3] = {{'1','2','3'},
+	           {'4','5','6'},
+	           {'7','8','9'}};
+
+string player1;
+string player2;
+int p_input;
+int row;
+int col;
 
 void Banner()
 {
@@ -59,7 +62,7 @@ void Board()
 
 void plotInput(int input,int player)
 {
-	if(player==1)
+	if (player==1)
 		board[row][col]='O';
 	else
 		board[row][col]='X';
@@ -71,95 +74,95 @@ bool checkInput(int input)
 {
 	row=input/3;
 	col=(input%3)-1;
-	if(input<1 || input>9)
+	if (input<1 || input>9) 
+
 	{
-		cout<<"Invalid input"<<endl;
+		cout<<"Invalid input!!"<<endl;
 		cout<<"Please enter the position again."<<endl;
 		return false;
 	}
 
-	if(board[row][col]=='X' || board[row][col]=='O')
+	if (board[row][col]=='X' || board[row][col]=='O')
 	{
-		cout<<"Already occupied"<<endl;
+		cout<<"Already occupied."<<endl;
 		return false;
 	}
 	return true;
 }
 
+void playerTurn(int player_id)
+{
+
+	bool valid_Input=false;
+	cout<<endl;
+	if (player_id==1)
+		cout<<player1<<", enter the position of your marker(O):"<<endl;
+	else
+		cout<<player2<<", enter the position of your marker(X):"<<endl;  
+	
+	while (1)
+	{
+		cin>>p_input;
+		valid_Input=checkInput(p_input);
+		if (valid_Input)
+			break;
+	}
+	plotInput(p_input,player_id);    
+
+}		
 
 void takeUserInput(int c)
-{
-	bool valid_Input=false;
-	if(c%2!=0)
-	{
-		cout<<endl;
-		cout<<player1<<" ,enter the position of your marker(O):"<<endl;
-		while(1)
-		{
-			cin>>p1_input;
-			valid_Input=checkInput(p1_input);
-			if(valid_Input)
-				break;
-		}
-		plotInput(p1_input,1);
-	}
+{       
+
+	if (c%2!=0)
+	        playerTurn(1);
 	else
-	{
-		cout<<endl;
-		cout<<player2<<" ,enter the position of your marker(X):"<<endl;
-		while(1)
-		{
-			cin>>p2_input;
-			valid_Input=checkInput(p2_input);
-			if(valid_Input)
-				break;
-		}
-		plotInput(p2_input,2);
-	}
+	        playerTurn(2);
+
 }
 
 
 bool checkForWinner()
-{
+{       
 	char marker;
 	bool winner;
 	string player;
-	if(board[0][0]==board[0][1] && board[0][2]==board[0][1] )
+	if (board[0][0]==board[0][1] && board[0][2]==board[0][1] )
 	{
 		winner=true;
 		marker=board[0][0];
 	}
-	else if(board[0][0]==board[1][0] && board[1][0]==board[2][0])  
+	else if (board[0][0]==board[1][0] && board[1][0]==board[2][0])  
 	{
 		winner=true;
 		marker=board[0][0];
 	}
-	else if(board[0][0]==board[1][1] && board[1][1]==board[2][2] )
+	else if (board[0][0]==board[1][1] && board[1][1]==board[2][2] )
 	{
 		winner=true;
 		marker=board[0][0];
 	}
-	else if(board[2][0]==board[2][1] && board[2][2]==board[2][1])   
+	else if (board[2][0]==board[2][1] && board[2][2]==board[2][1])   
 	{
 		winner=true;
 		marker=board[2][0];
 	}
-	else if( board[0][2]==board[1][2] && board[2][2]==board[1][2])
+	else if ( board[0][2]==board[1][2] && board[2][2]==board[1][2])
 	{
 		winner=true;
 		marker=board[0][2];
 	}
-	else if(board[2][0]==board[1][1] && board[0][2]==board[1][1])
+	else if (board[2][0]==board[1][1] && board[0][2]==board[1][1])
 	{
 		winner=true;
 		marker=board[2][0];
 	}  
-	else if(board[0][1]==board[1][1] && board[2][1]==board[1][1])
+	else if (board[0][1]==board[1][1] && board[2][1]==board[1][1])
 	{
 		winner=true;
 		marker=board[0][1];
 	}
-	else if(board[1][0]==board[1][1]  && board[1][2]==board[1][1])
+	else if (board[1][0]==board[1][1]  && board[1][2]==board[1][1])
 	{
 		winner=true;
 		marker=board[1][0];
@@ -168,14 +171,15 @@ bool checkForWinner()
 	{
 		winner=false;
 	}
-	if(winner)
+	if (winner)
 	{
 		if(marker=='O')
 			player=player1;
 		else
 			player=player2;
+		cout<<player<<" has won the game."<<endl;
 	}
-	cout<<player<<" has won the game."<<endl;
+
 	return(winner);
 }
 
@@ -193,14 +197,19 @@ int main()
 	cout<<endl;
 	cin>>player2;
 	cout<<endl;
-	while(1)
+        
+        while (1)
 	{
+		
 		Board();
-		if(count>9)
+
+		if (count>9)
 			break;
+
 		takeUserInput(count);
 		winnerFound=checkForWinner();
-		if(winnerFound)
+
+		if (winnerFound)
 		{
 			Board();
 			break;
@@ -209,7 +218,9 @@ int main()
 
 
 	}
-	if(winnerFound==false)
-		cout<<"It's a draw"<<endl;
+	
+	if (winnerFound==false)
+		cout<<"It's a draw."<<endl;
+	
 	return 0;
 }
